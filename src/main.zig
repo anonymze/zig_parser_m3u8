89362812a45cf668b0d7e4a67ccdc1d013e74aba
anonymze <anodevfr@gmail.com>
2024-09-19 15:59:19 +0200

    WE DID IT ! PARSING AND ENCODING TO MP4

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const http = std.http;
+const fs = std.fs;
 const Uri = std.Uri;
+const Allocator = std.mem.Allocator;
+const Child = std.process.Child;
 const eql = std.mem.eql;
 const print = std.debug.print;
 const expect = std.testing.expect;
@@ -8,6 +11,9 @@ const expect = std.testing.expect;
 const BASE_URL = "https://mayi22140.mayicloud.com/files/aa/";
 const FILE = "kYo4hQwhOQOF7UvF8LSDcJb6xP3wVBKfA8k.m3u8";
 const HEADER_TYPE = "application/vnd.apple.mpegurl";
+const FFMPEG_COMMAND = [_][]const u8{ "ffmpeg", "-protocol_whitelist", "file,http,https,tcp,tls,crypto", "-f", "concat", "-safe", "0", "-i", "file.txt", "-c", "copy", "output.mp4" };
+
+const FetchFileError = Uri.ParseError || error{ IncorrectHeader, IncorrectResponseStatus, UrlConnectionError };
 
 pub fn main() !void {
     // create an allocator (will be needed for things we can't know the size)
@@ -15,33 +21,21 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     var allocator = gpa.allocator();
 
-    const body_m3u8 = try fetchFile(BASE_URL ++ FILE, allocator, HEADER_TYPE);
+    const body_m3u8 = fetchFile(BASE_URL ++ FILE, allocator, HEADER_TYPE) catch |err| {
+        return logError(err);
+    };
+
     defer allocator.free(body_m3u8);
     // print("Content:\n{s}\n", .{body_m3u8});
 
     const list = try readContentFile(body_m3u8, allocator);
     defer allocator.free(list);
 
-    std.debug.print("{s}", .{list});
-
-    for (list) |file| {
-        var buffer_url = try allocator.alloc(u8, BASE_URL.len + file.len);
-
-        // sliced last index is not inclusive
-        std.mem.copyForwards(u8, buffer_url[0..BASE_URL.len], BASE_URL);
-        std.mem.copyForwards(u8, buffer_url[BASE_URL.len..], file);
-
-        const body_ts = try fetchFile(buffer_url, allocator, null);
-
-        allocator.free(buffer_url);
-        allocator.free(body_ts);
-
-        // TODO
-        break;
-    }
+    try createFileAndPopulate(list, allocator);
+    try startFfmpegCommand();
 }
 
-fn readContentFile(body: []u8, allocator: std.mem.Allocator) ![][]u8 {
+fn readContentFile(body: []u8, allocator: Allocator) ![][]u8 {
     const page_alloc = std.heap.page_allocator;
 
     var list = std.ArrayList([]u8).init(allocator);
@@ -70,7 +64,7 @@ fn readContentFile(body: []u8, allocator: std.mem.Allocator) ![][]u8 {
     return try list.toOwnedSlice();
 }
 
-fn fetchFile(url: []const u8, allocator: std.mem.Allocator, forced_header: ?[]const u8) ![]u8 {
+fn fetchFile(url: []const u8, allocator: Allocator, forced_header: ?[]const u8) (FetchFileError || Allocator.Error)![]u8 {
     // create an HTTP client.
     var client = http.Client{ .allocator = allocator };
     defer client.deinit();
@@ -83,36 +77,113 @@ fn fetchFile(url: []const u8, allocator: std.mem.Allocator, forced_header: ?[]co
     defer allocator.free(buff);
 
     // open a connection
-    var req = try client.open(.GET, uri, .{
+    var req = client.open(.GET, uri, .{
         // you have to handle the allocation size of the entire header yourself, so we give the buffer_url created before
         .server_header_buffer = buff,
-    });
+    }) catch return FetchFileError.UrlConnectionError;
+
     defer req.deinit();
 
     // send request headers
-    try req.send();
+    req.send() catch unreachable;
+
     // finish the body of the request
-    try req.finish();
+    // req.finish() catch unreachable;
+
     // wait response returned
-    try req.wait();
+    req.wait() catch unreachable;
 
     var iter = req.response.iterateHeaders();
     while (iter.next()) |header| {
         // print("Name:{s}, Value:{s}\n", .{ header.name, header.value });
 
         if (forced_header) |value| {
-            // TODO boolean
             if (eql(u8, header.name, "Content-Type") and !eql(u8, header.value, value)) {
-                std.log.err("Header Content-Type. It needs to be '{?s}'", .{forced_header});
-                // @panic("Wrong header");
+                return FetchFileError.IncorrectHeader;
             }
         }
     }
 
-    try std.testing.expectEqual(req.response.status, .ok);
+    if (req.response.status != .ok) return FetchFileError.IncorrectResponseStatus;
+    // std.testing.expectEqual(req.response.status, .ok) catch return FetchFileError.IncorrectResponseStatus;
 
     var readerBody = req.reader();
-    const body = try readerBody.readAllAlloc(allocator, 1024 * 1024 * 4);
+    const body = readerBody.readAllAlloc(allocator, 1024 * 1024 * 4) catch return Allocator.Error.OutOfMemory;
 
     return body;
+}
+
+fn logError(err: anyerror) !void {
+    switch (err) {
+        // FETCH
+        FetchFileError.IncorrectHeader => {
+            std.log.err("Header Content-Type. It needs to be '{s}'", .{HEADER_TYPE});
+        },
+        FetchFileError.IncorrectResponseStatus => {
+            std.log.err("The server response is not 200", .{});
+        },
+        FetchFileError.UrlConnectionError => {
+            std.log.err("The connection to the URL seems to fail", .{});
+        },
+        FetchFileError.InvalidFormat => {
+            std.log.err("The URL is not valid URL", .{});
+        },
+        FetchFileError.UnexpectedCharacter => {
+            std.log.err("Unexpected character in the URL", .{});
+        },
+        FetchFileError.InvalidPort => {
+            std.log.err("The URL port is not valid", .{});
+        },
+        // ALLOCATOR
+        Allocator.Error.OutOfMemory => {
+            std.log.err("Out of memory. Request or headers may be too big or computer not enough space", .{});
+        },
+        // OTHER
+        else => {
+            std.log.err("Error not handled : {}", .{err});
+        },
+    }
+
+    return;
+}
+
+fn createFileAndPopulate(content: [][]u8, allocator: Allocator) !void {
+    const cwd = try fs.cwd().realpathAlloc(allocator, ".");
+    std.debug.print("{s}", .{cwd});
+    allocator.free(cwd);
+
+    const file_ts = try fs.Dir.createFile(fs.cwd(), "file.txt", .{});
+    defer file_ts.close();
+
+    const writer = file_ts.writer();
+
+    for (content) |file| {
+        var buffer_url = try allocator.alloc(u8, BASE_URL.len + file.len);
+
+        // sliced last index is not inclusive
+        std.mem.copyForwards(u8, buffer_url[0..BASE_URL.len], BASE_URL);
+        std.mem.copyForwards(u8, buffer_url[BASE_URL.len..], file);
+
+        try writer.writeAll("file '");
+        try writer.writeAll(buffer_url);
+        try writer.writeAll("'\n");
+
+        allocator.free(buffer_url);
+    }
+}
+
+fn startFfmpegCommand() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const argv = FFMPEG_COMMAND;
+
+    // by default, child will inherit stdout & stderr from its parents
+    // you can collect them out and show it the way you want. I.E : https://cookbook.ziglang.cc/08-02-external.html
+    var child = Child.init(&argv, allocator);
+
+    const term = try child.spawnAndWait();
+
+    try std.testing.expectEqual(term.Exited, 0);
 }
